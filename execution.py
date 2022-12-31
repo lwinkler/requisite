2a5864e93d42262925a0f4f449405312c9b4284c
Laurent Winkler <laurent.winkler@3hle.ch>
2022-12-31 23:53:17 +0100

    refactor: split

---
+++ execution.py
@@ -0,0 +1,60 @@
+"""Code related to test execution"""
+
+from datetime import datetime
+from typing import List
+
+import entries as en
+import operations as op
+
+
+def datetime_to_string(dt: datetime) -> str:
+    """Convert datetime into a string"""
+    return dt.strftime("%Y-%m-%d.%H%M%S")
+
+
+class TestExecution(en.Entry):
+    """The execution of a test"""
+
+    short_type = "ex"
+    yaml_tag = "!TestExecution"
+
+    def __init__(self, test_id: str, date: str, result: str):
+        super().__init__("", "", [])  # TODO: is there a better way ?
+        self.test_id = test_id
+        self.date = date
+        self.result = result
+
+
+class TestListExecution(en.Entry):
+    """The execution of a test list"""
+
+    short_type = "lex"
+    yaml_tag = "!TestListExecution"
+
+    def __init__(
+        self, test_list_id: str, date: str, children: List[en.Entry], result: str
+    ):
+        super().__init__("", "", children)  # TODO: is there a better way ?
+        self.test_list_id = test_list_id
+        self.date = date
+        self.result = result
+
+
+class TestEngine:  # (en.Entry):
+
+    # short_type = "ten"
+    # yaml_tag = "!TestEngine"
+
+    def run_test_list(self, test_list: en.TestList) -> List[TestExecution]:
+        """Run the tests of a test list"""
+        results: List[TestExecution] = []
+        for test in op.extract_entries_of_type(test_list, en.Test):
+            result = self.run_test(test)
+            results.append(
+                TestExecution(test.get_id(), datetime_to_string(datetime.now()), result)
+            )
+        return results
+
+    def run_test(self, test: en.Test) -> str:
+        """Run one test"""
+        return "TODO"
--- release.py
+++ release.py
@@ -5,15 +5,14 @@
 import os
 import sys
 import argparse
-import yaml
 from datetime import datetime
 from pathlib import Path
-from typing import List
 
 import yaml_util as yu
 import entries as en
 import operations as op
 import expanders
+import execution as ex
 import rules as ru
 import report as rp
 import verification as ve
@@ -48,7 +47,8 @@ def arguments_parser() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "-d", "--test_dir",
+        "-d",
+        "--test_dir",
         type=Path,
         help="The test directory. If empty it will be generated.",
     )
@@ -64,57 +64,13 @@ def check_for_errors(design1: en.Design) -> None:
     if errors:
         sys.exit(1)
 
-def datetime_to_string(dt: datetime) -> str:
-    """Convert datetime into a string"""
-    return dt.strftime('%Y-%m-%d.%H%M%S')
 
 def generate_test_dir_path(releases_path: Path) -> Path:
     """Generate the test dir path with current time stamp"""
-    return releases_path / datetime_to_string(datetime.now())
+    return releases_path / ex.datetime_to_string(datetime.now())
 
 
 # -----
-
-class TestExecution(en.Entry):
-    """The execution of a test"""
-
-    short_type = "ex"
-    yaml_tag = "!TestExecution"
-
-    def __init__(self, test_id: str, date: str, result: str):
-        super().__init__("", "", []) # TODO: is there a better way ?
-        self.test_id = test_id
-        self.date = date
-        self.result = result
-
-class TestListExecution(en.Entry):
-    """The execution of a test list"""
-
-    short_type = "lex"
-    yaml_tag = "!TestListExecution"
-
-    def __init__(self, test_list_id: str, date: str, children: List[TestExecution], result: str):
-        super().__init__("", "", children) # TODO: is there a better way ?
-        self.test_list_id = test_list_id
-        self.date = date
-        self.result = result
-
-class TestEngine: # (en.Entry):
-
-    # short_type = "ten"
-    # yaml_tag = "!TestEngine"
-
-    def run_test_list(self, test_list: en.TestList) -> List[TestExecution]:
-        """Run the tests of a test list"""
-        results : List[TestExecution] = []
-        for test in op.extract_entries_of_type(test_list, en.Test):
-            result = self.run_test(test)
-            results.append(TestExecution(test.get_id(), datetime_to_string(datetime.now()), result))
-        return results
-
-    def run_test(self, test: en.Test) -> str:
-        """Run one test"""
-        return "TODO"
 
 
 if __name__ == "__main__":
@@ -125,9 +81,10 @@ if __name__ == "__main__":
     design.expand(design, None)
     check_for_errors(design)
 
-
     releases_path = Path(".") / "releases"
-    test_directory = args.test_dir if args.test_dir else generate_test_dir_path(releases_path)
+    test_directory = (
+        args.test_dir if args.test_dir else generate_test_dir_path(releases_path)
+    )
     if not test_directory.is_dir():
         print(f"Create output directory {test_directory.as_posix()}")
         if not releases_path.is_dir():
@@ -139,18 +96,17 @@ if __name__ == "__main__":
     # TODO: Add generation/expansion date to design
     assert releases_path in test_directory.parents
 
-
-
     verifier = ve.Verifier(design)
     # unverified = verifier.list_unverified(design)
 
-    engine = TestEngine()
+    # TODO: Move this code ?
+    engine = ex.TestEngine()
     for entry in op.extract_entries_of_type(design, en.TestList):
-        test_executions = engine.execute_test_list(entry)
-        test_list_execution = TestListExecution(entry.get_id(), datetime_to_string(datetime.now()), test_executions, "TODO")
+        test_executions = engine.run_test_list(entry)
+        test_list_execution = ex.TestListExecution(
+            entry.get_id(), ex.datetime_to_string(datetime.now()), test_executions, "TODO"
+        )
         yu.write_entry(test_directory / (entry.get_id() + ".yaml"), test_list_execution)
 
-
-    
-    print(f"Create report")
+    print("Create report")
     rp.write_html_report(test_directory / "report.html", design, verifier)
